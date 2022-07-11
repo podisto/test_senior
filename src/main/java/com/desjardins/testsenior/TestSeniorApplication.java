1e4365615ad1d93804933c10ee044f44ab3dd617
Yacine Diop <yacinediop@Yacines-MacBook-Pro.local>
2022-07-11 14:13:16 -0400

    initial commit

---
+++ src/main/java/com/desjardins/testsenior/TestSeniorApplication.java
@@ -0,0 +1,13 @@
+package com.desjardins.testsenior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSeniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSeniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desjardins/testsenior/TestSeniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desjardins.testsenior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSeniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
